90f4619520ba89e6959f61c735bec8f26220a101
Njodsa <31994505+Njodsa@users.noreply.github.com>
2019-02-23 13:01:59 +0300

    fix: column references user_id in group_user table

--- database/migrations/2019_02_21_081959_create_group_user_table.php
+++ database/migrations/2019_02_21_081959_create_group_user_table.php
@@ -16,7 +16,7 @@ class CreateGroupUserTable extends Migration
       Schema::create('group_user', function (Blueprint $table) {
         $table->integer('user_id')->unsigned();
         $table->integer('group_id')->unsigned();
-        $table->foreign('user_id')->references('id')->on('groups')->onDelete('cascade');
+        $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
       });
     }
