0d4b05bd73a87bda06eced908226a3ae64a197c1
Njodsa <31994505+Njodsa@users.noreply.github.com>
2019-03-04 16:21:38 +0300

    create reportTable file for non-repetition

--- resources/views/report/ListOfReports.blade.php
+++ resources/views/report/ListOfReports.blade.php
@@ -6,52 +6,5 @@
     <a style="float: left;"class="btn btn-success btn-sm" href="{{route('report.create')}}"> إضافة</a>
   @endrole
   @endslot
-  <div class="table-responsive">
-    <table class="table table-bordered">
-      <thead>
-        <tr>
-          <th>اسم التقرير</th>
-          <th>المجموعة</th>
-          <th>التصنيف  </th>
-          <th>تاريخ الإنشاء </th>
-          <th colspan="3"> الإجراء </th>
-        </tr>
-     </thead>
-     <tbody>
-       @foreach($reports as $report)
-          <tr>
-            <td>{{$report->name}} </td>
-            <td>{{$report->group->name}}</td>
-            <td>
-              @foreach($report->tags as $tag)
-                {{ $loop->first ? '' : ', ' }}
-                {{$tag}}
-              @endforeach
-            </td>
-            <td>{{$report->created_at}}</td>
-            <td>
-              <a class="btn btn-success btn-sm" href="{{route('report.show',[$report->id])}}"> عرض</a>
-            </td>
-            @role(['Admin','Editor'])
-              <td>
-                <a class="btn btn-info btn-sm"href="{{route('report.edit', [$report->id])}}">تعديل </a>
-              </td>
-            @endrole
-            @role(['Delete','Admin'])
-              <td>
-                <form method="post" action="{{route('report.destroy' ,[ $report->id ])}}">
-                  @method('DELETE')
-                  @csrf
-                  <button type="submit" class="btn btn-danger btn-sm"
-                     onclick="return confirm('هل انت متاكد من حذف التقرير ?');">
-                     حذف
-                  </button>
-              </td>
-            @endrole
-           </tr>
-      @endforeach
-     </tbody>
-    </table>
-  </div>
-    {{ $reports->links() }}
+  @include('layouts.inc.report.reportTable')
 @endcomponent
