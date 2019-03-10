bbbd2ade777e70e58e9788f0fb38ec0579a8ce10
Njodsa <31994505+Njodsa@users.noreply.github.com>
2019-03-10 21:56:43 +0300

    edit roleMiddleware if uaser has role or admin

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -15,7 +15,7 @@ class RoleMiddleware
      */
     public function handle($request, Closure $next, ...$role)
     {
-      if(!$request->user()->hasRole($role)) {
+      if(!$request->user()->hasRole($role) && !$request->user()->isAdmin()) {
           abort(404);
        }
       return $next($request);
